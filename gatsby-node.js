8f821f80ea412407f0b2ce46bcce36964f333d1b
Kyle Pfromer <kpfromer2@gmail.com>
2020-06-22 20:37:29 -0600

    feat: update blog pages for forestry

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,34 +1,76 @@
-const path = require("path")
+const path = require(`path`)
+const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  const blogPost = path.resolve(`./src/templates/BlogPost.tsx`)
   const result = await graphql(
     `
       {
-        allWordpressPost {
+        allMdx(
+          sort: { fields: [frontmatter___date], order: DESC }
+          limit: 1000
+        ) {
           edges {
             node {
               id
-              slug
+              fields {
+                blogPath
+                slug
+              }
+              frontmatter {
+                title
+              }
             }
           }
         }
       }
     `
   )
-  const posts = result.data.allWordpressPost.edges
-  const { createPage } = actions
-  const blogPost = path.resolve(`./src/templates/BlogPost.tsx`)
-  //   // Create blog posts pages.
+
+  if (result.errors) {
+    throw result.errors
+  }
+
+  // Create blog posts pages.
+  const posts = result.data.allMdx.edges
+
   posts.forEach((post, index) => {
-    // const previous = index === posts.length - 1 ? null : posts[index + 1].node
-    // const next = index === 0 ? null : posts[index - 1].node
-    // const { id } = post.node.id
+    const previous = index === posts.length - 1 ? null : posts[index + 1].node
+    const next = index === 0 ? null : posts[index - 1].node
+
+    const { slug, blogPath } = post.node.fields
+
     createPage({
-      path: `/blog/${post.node.slug}`,
+      path: blogPath,
       component: blogPost,
       context: {
         id: post.node.id,
+        slug,
+        blogPath,
+        previous,
+        next,
       },
     })
   })
+}
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === "Mdx") {
+    const value = createFilePath({ node, getNode })
+    console.log(value)
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+    createNodeField({
+      name: `blogPath`,
+      node,
+      value: `/blog${value}`,
+    })
+  }
 }
